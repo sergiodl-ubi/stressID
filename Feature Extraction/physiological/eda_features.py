0aa82b89a187ad9829c71451c3b2c0172f5448ff
Sergio De León <sergio.deleon@ubi-lab.com>
2025-06-18 21:48:27 +0900

    Updating code for compatibility with new versions

--- Feature Extraction/physiological/eda_features.py
+++ Feature Extraction/physiological/eda_features.py
@@ -1,4 +1,8 @@
+# pyright: reportUnknownParameterType=false, reportMissingParameterType=false, reportUnknownArgumentType=false, reportUnknownMemberType=false, reportAny=false, reportUnknownVariableType=false
+
+from ntpath import samefile
 import numpy as np
+from optype.numpy import AnyStrArray
 import scipy.io
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,19 +11,16 @@ from pathlib import Path
 import scipy.stats as stats
 import neurokit2 as nk
 
-
+from numpy.typing import ArrayLike
 import warnings
 warnings.filterwarnings('ignore')
 
 
 ############################ STAT FEATURES ##############################
 
-def eda_stat(array, sampling_freq=1000):
-    # Input: Arrays (numpy, dataframe, list) 
-    # -> Output : A pd.DataFrame
-    
+def eda_stat(array: ArrayLike, sampling_freq: int=1000) -> pd.DataFrame:
     x = np.array(array)
-    eda = nk.eda_phasic(x, sampling_freq)
+    eda = nk.eda_phasic(eda_signal=x, sampling_rate=sampling_freq)
     scr = np.array(eda['EDA_Phasic'])
     scl = np.array(eda['EDA_Tonic'])
     x_axis = np.linspace(0, scl.shape[0]/sampling_freq, scl.shape[0])
@@ -31,16 +32,14 @@ def eda_stat(array, sampling_freq=1000):
                               scl.max(), scl.min(), scl.mean(), scl.std()]).T
 
     df.columns = ['max_eda', 'min_eda', 'mean_eda', 'sd_eda', 'ku_eda', 'sk_eda', 'median_eda',
-                  'dynrange','scl_slope', 'max_scr', 'min_scr', 'mean_scr', 'sd_scr', 'max_scl', 
-                  'min_scl', 'mean_scl', 'sd_scl']
+                  'dynrange','scl_slope', 'max_scr', 'min_scr', 'mean_scr', 'sd_scr',
+                  'max_scl', 'min_scl', 'mean_scl', 'sd_scl']
     
     return df
-    
-############################ TIME FEATURES ##############################    
-    
-def eda_time(array, sampling_freq=1000):
-    # Input: An array (numpy, dataframe, list) 
-    # -> Output : A pd.DataFrame
+
+############################ TIME FEATURES ##############################
+
+def eda_time(array: ArrayLike, sampling_freq: int=1000) -> pd.DataFrame:
     x = np.array(array)
     eda = nk.eda_phasic(x, sampling_freq)
     scr = np.array(eda['EDA_Phasic'])
@@ -51,7 +50,7 @@ def eda_time(array, sampling_freq=1000):
     recovery = info['SCR_RecoveryTime']
     
     nSCR = len(info['SCR_Peaks']) / (x.shape[0]/sampling_freq/60) #to get the number of peaks per minute
-    aucSCR = np.trapz(scr)
+    aucSCR = np.trapezoid(scr)
     meanAmpSCR = np.nanmean(amplitude)
     maxAmpSCR = np.nanmax(amplitude)
     meanRespSCR = np.nanmean(recovery)
@@ -65,14 +64,11 @@ def eda_time(array, sampling_freq=1000):
     df.columns = ['nSCR', 'aucSCR', 'meanAmpSCR', 'maxAmpSCR', 'meanRespSCR',
                   'sumAmpSCR', 'sumRespSCR']
     return df
-    
 
+############################ DATABASE ##############################
 
-############################ DATABASE ##############################    
-    
-
-def eda_stat_features(dictionnary, sampling_freq =1000):
-    data = dictionnary.copy()
+def eda_stat_features(dictionary, sampling_freq: int=1000):
+    data = dictionary.copy()
     df_stat = eda_stat(list(data.values())[0], sampling_freq)
     names = [list(data.keys())[0]]
     
@@ -87,8 +83,8 @@ def eda_stat_features(dictionnary, sampling_freq =1000):
     return df_stat
 
 
-def eda_time_features(dictionnary, sampling_freq =1000):
-    data = dictionnary.copy()
+def eda_time_features(dictionary, sampling_freq =1000):
+    data = dictionary.copy()
     df_time = eda_time(list(data.values())[0], sampling_freq)
     names = [list(data.keys())[0]]
     
@@ -102,9 +98,9 @@ def eda_time_features(dictionnary, sampling_freq =1000):
     df_time.set_axis(names, inplace=True)
     return df_time
 
-def get_eda_features(dictionnary, sampling_freq =500):    
-    df_stat = eda_stat_features(dictionnary, sampling_freq)
-    df_time = eda_time_features(dictionnary, sampling_freq)
+def get_eda_features(dictionary, sampling_freq =500):
+    df_stat = eda_stat_features(dictionary, sampling_freq)
+    df_time = eda_time_features(dictionary, sampling_freq)
     
     df = pd.concat([df_stat,df_time], axis=1)
     return df
