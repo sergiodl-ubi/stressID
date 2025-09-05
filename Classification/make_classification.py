42013022d90a2a94c3e7d1551b0ebdfaf518840a
Sergio De León <sergio.deleon@ubi-lab.com>
2025-09-05 17:14:25 +0900

    Adding upstream changes from uxfrustrationrecog branch

--- Classification/make_classification.py
+++ Classification/make_classification.py
@@ -3,7 +3,7 @@ import scipy.stats as stats
 
 import pandas as pd
 import random
-import time 
+import time
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
@@ -35,22 +35,22 @@ def make_nclassif(X, y, n_splits=10, feature_selector=None, list_classifiers=Non
     # Dictionnary to store f1-score and accuracy
     df_res = pd.DataFrame({'n':[],'f1-score':[],'accuracy':[], 'classifier':[], 'time':[]})
     conf_matrices = []
-    
+
     imputer = IterativeImputer(random_state=0) if impute else None
     scaler = StandardScaler() if scale else None
-    
+
     # Defaut classifiers tested: Logistic regression, Random Forests, Adaboost
     if not list_classifiers :
         list_classifiers = [LogisticRegression(max_iter=2000),
                             RandomForestClassifier(max_depth=5),
                             AdaBoostClassifier(n_estimators=100)]
-                       
-            
+
+
     # Make n-group random splits grouped by subjects
     groups = [l.split('_')[0] for l in list(y.index)]
-    
+
     #rstate = random.randint(0,100)
-    
+
     X_shuffled, y_shuffled, groups_shuffled = shuffle(X, y, groups)
 
     group_kfold = GroupKFold(n_splits=n_splits)
@@ -62,145 +62,145 @@ def make_nclassif(X, y, n_splits=10, feature_selector=None, list_classifiers=Non
         x_test = X.iloc[test_index]
         y_train = y.iloc[train_index]
         y_test = y.iloc[test_index]
-        
+
         if verbose:
             print('Split {0:2d}/{1:2d}'.format(s+1, n_splits))
-        
+
         # Fit each model
         for model in list_classifiers:
 
-            if feature_selector == 'l1':  
+            if feature_selector == 'L1':
                 clf = Pipeline([
-                    ('impute',imputer), 
-                    ('scale', scaler), 
+                    ('impute',imputer),
+                    ('scale', scaler),
                     ('feature_selection', SelectFromModel(LogisticRegression(max_iter=5000,
-                                                                             C=0.1, 
-                                                                             penalty="l1", 
-                                                                             dual=False, 
+                                                                             C=0.1,
+                                                                             penalty="l1",
+                                                                             dual=False,
                                                                              solver='saga'))),
                     ('classification', model)
                 ])
             elif feature_selector == 'RFECV':
                 clf = Pipeline([
-                    ('impute',imputer), 
-                    ('scale', scaler), 
+                    ('impute',imputer),
+                    ('scale', scaler),
                     ('feature_selection', RFECV(RandomForestClassifier(max_depth=5),
-                                                step=2, 
+                                                step=2,
                                                 cv=2)),
                     ('classification', model)
                 ])
             elif feature_selector == 'RFE':
                     clf = Pipeline([
-                        ('impute',imputer), 
-                        ('scale', scaler), 
+                        ('impute',imputer),
+                        ('scale', scaler),
                         ('feature_selection', RFE(RandomForestClassifier(max_depth=5),
                                                     n_features_to_select=20, step=2)),
                         ('classification', model)
                     ])
             else:
                 clf = Pipeline([
-                    ('impute',imputer), 
-                    ('scale', scaler), 
+                    ('impute',imputer),
+                    ('scale', scaler),
                     ('classification', model)
                 ])
-                
+
             tic = time.perf_counter()
             clf.fit(x_train, y_train)
             toc = time.perf_counter()
-            
+
             # Retrieve accuracy and F1-score
             y_pred = clf.predict(x_test)
             conf_matrices.append(confusion_matrix(y_test, y_pred))
 
             new_row = {'n':int(s),'f1-score':f1_score(y_test, y_pred, average='weighted'),
-                                    'accuracy':balanced_accuracy_score(y_test, y_pred), 
+                                    'accuracy':balanced_accuracy_score(y_test, y_pred),
                                     'classifier':model.__class__.__name__, 'time':toc-tic}
-        
-            df_res.loc[len(df_res)] = new_row 
+
+            df_res.loc[len(df_res)] = new_row
 
 
-    
+
     return df_res, conf_matrices
-    
-   
-##########################################################################################  
+
+
+##########################################################################################
 
 
 def avg_res(res):
     return res.groupby(['classifier']).mean()[['f1-score', 'accuracy', 'time']]
-    
-    
 
 
-##########################################################################################    
-       
+
+
+##########################################################################################
+
 def make_nclassif_random_splits(
     X, y, n_splits=10,
     feature_selector=None, list_classifiers=None, impute=True, scale=True,
-    verbose=True, random_seed: int|None = None):
+    verbose=True, random_seed: int|None = None, test_size: float = 0.2):
     # Dictionnary to store f1-score and accuracy
     df_res= pd.DataFrame({'n':[],'f1-score':[],'accuracy':[], 'classifier':[], 'time':[]})
     conf_matrices = []
-    
+
     imputer = IterativeImputer() if impute else None
     scaler = StandardScaler() if scale else None
-    
+
     # Defaut classifiers tested: Logistic regression, Random Forests, Adaboost
     if not list_classifiers :
         list_classifiers = [LogisticRegression(max_iter=2000),
                             RandomForestClassifier(max_depth=5),
                             AdaBoostClassifier(n_estimators=100)]
-                        
-    # Make n random splits 
+
+    # Make n random splits
     for s in range(n_splits):
-        x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=random_seed)
-        
+        x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_seed)
+
         if verbose:
             print('Split {0:2d}/{1:2d}'.format(s+1, n_splits))
-        
+
         # Fit each model
         for model in list_classifiers:
 
-            if feature_selector == 'l1':  
+            if feature_selector == 'L1':
                 clf = Pipeline([
-                    ('impute',imputer), 
-                    ('scale', scaler), 
+                    ('impute',imputer),
+                    ('scale', scaler),
                     ('feature_selection', SelectFromModel(LogisticRegression(max_iter=5000,
-                                                                             C=0.1, 
-                                                                             penalty="l1", 
-                                                                             dual=False, 
+                                                                             C=0.1,
+                                                                             penalty="l1",
+                                                                             dual=False,
                                                                              solver='saga'))),
                     ('classification', model)
                 ])
             elif feature_selector == 'RFE':
                 clf = Pipeline([
-                    ('impute',imputer), 
-                    ('scale', scaler), 
+                    ('impute',imputer),
+                    ('scale', scaler),
                     ('feature_selection', RFECV(RandomForestClassifier(max_depth=5),
-                                                step=2, 
+                                                step=2,
                                                 cv=2)),
                     ('classification', model)
                 ])
             elif feature_selector == 'PCA':
                 pca = PCA(n_components=0.95, svd_solver='full')
-                
+
                 clf = Pipeline([
-                    ('impute',imputer), 
-                    ('scale', scaler), 
+                    ('impute',imputer),
+                    ('scale', scaler),
                     ('pca', pca),
                     ('classification', model)
                 ])
             else:
                 clf = Pipeline([
-                    ('impute',imputer), 
-                    ('scale', scaler), 
+                    ('impute',imputer),
+                    ('scale', scaler),
                     ('classification', model)
                 ])
-                
+
             tic = time.perf_counter()
             clf.fit(x_train, y_train)
             toc = time.perf_counter()
-            
+
             # Retrieve accuracy and F1-score
             y_pred = clf.predict(x_test)
             conf_matrices.append(confusion_matrix(y_test, y_pred))
@@ -209,17 +209,17 @@ def make_nclassif_random_splits(
                 modelname = model.__class__.__name__ + suffix
             else :
                 modelname = model.__class__.__name__
-            
-            
-            
+
+
+
             new_row = {'n':int(s),'f1-score':f1_score(y_test, y_pred, average='weighted'),
-                                    'accuracy':balanced_accuracy_score(y_test, y_pred), 
+                                    'accuracy':balanced_accuracy_score(y_test, y_pred),
                                     'classifier':modelname, 'time':toc-tic}
-            df_res.loc[len(df_res)] = new_row 
-    
+            df_res.loc[len(df_res)] = new_row
+
     return df_res, conf_matrices
 
-##########################################################################################  
+##########################################################################################
 
 
 
@@ -232,28 +232,28 @@ def make_nclassif_random_splits_resample(X, y, n_splits=10, resamp = 'SMOTE', fe
     conf_matrices = []
     y_preds = []
     y_tests = []
-    
+
     if impute:
         imputer = IterativeImputer()
     else:
         imputer = None
-        
+
     if scale:
         scaler = StandardScaler()
     else:
         scaler = None
-    
-    
+
+
     # Defaut classifiers tested: Logistic regression, Random Forests, Adaboost
     if not list_classifiers :
         list_classifiers = [LogisticRegression(max_iter=2000),
                             RandomForestClassifier(max_depth=5),
                             AdaBoostClassifier(n_estimators=100)]
-                        
-    # Make n random splits 
-    for s in range(n_splits):        
+
+    # Make n random splits
+    for s in range(n_splits):
         x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        
+
         y_high = pd.Series(y_train[y_train == 1])
         idx_high = list(x_train.merge(y_high, left_index= True, right_index=True).index)
         x_high = x_train.loc[idx_high]
@@ -265,65 +265,65 @@ def make_nclassif_random_splits_resample(X, y, n_splits=10, resamp = 'SMOTE', fe
         if resamp :
             oversample = SMOTE()
             x_train, y_train = oversample.fit_resample(x_train, y_train)
-            
+
 
         if verbose:
             print('Split {0:2d}/{1:2d}'.format(s+1, n_splits))
-        
+
         # Fit each model
         for i,model in enumerate(list_classifiers):
 
-            if feature_selector == 'l1':  
+            if feature_selector == 'L1':
                 clf = Pipeline([
-                    ('impute',imputer), 
-                    ('scale', scaler), 
+                    ('impute',imputer),
+                    ('scale', scaler),
                     ('feature_selection', SelectFromModel(LogisticRegression(max_iter=5000,
-                                                                             C=0.1, 
-                                                                             penalty="l1", 
-                                                                             dual=False, 
+                                                                             C=0.1,
+                                                                             penalty="l1",
+                                                                             dual=False,
                                                                              solver='saga'))),
                     ('classification', model)
                 ])
             elif feature_selector == 'RFE':
                 clf = Pipeline([
-                    ('impute',imputer), 
-                    ('scale', scaler), 
+                    ('impute',imputer),
+                    ('scale', scaler),
                     ('feature_selection', RFECV(RandomForestClassifier(max_depth=5),
-                                                step=2, 
+                                                step=2,
                                                 cv=2)),
                     ('classification', model)
                 ])
             elif feature_selector == 'PCA':
                 pca = PCA(n_components=0.95, svd_solver='full')
-                
+
                 clf = Pipeline([
-                    ('impute',imputer), 
-                    ('scale', scaler), 
+                    ('impute',imputer),
+                    ('scale', scaler),
                     ('pca', pca),
                     ('classification', model)
                 ])
             else:
                 clf = Pipeline([
-                    ('impute',imputer), 
-                    ('scale', scaler), 
+                    ('impute',imputer),
+                    ('scale', scaler),
                     ('classification', model)
                 ])
-                
+
             tic = time.perf_counter()
             clf.fit(x_train, y_train)
             toc = time.perf_counter()
-            
+
             # Retrieve accuracy and F1-score
             y_pred = clf.predict(x_test)
             conf_matrices.append(confusion_matrix(y_test, y_pred))
-            
+
             y_tests.append(pd.Series(y_test))
             y_preds.append(pd.Series(y_pred, index = y_test.index))
-            
+
             new_row = {'n':int(s),'f1-score':f1_score(y_test, y_pred, average='weighted'),
-                                    'accuracy':balanced_accuracy_score(y_test, y_pred), 
+                                    'accuracy':balanced_accuracy_score(y_test, y_pred),
                                     'classifier':model.__class__.__name__, 'time':toc-tic}
 
-            df_res.loc[len(df_res)] = new_row 
-    
+            df_res.loc[len(df_res)] = new_row
+
     return df_res, conf_matrices, y_preds, y_tests
